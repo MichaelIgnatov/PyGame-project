aaf7a74d908acb47edee95a933c8cd187675a853
MichaelIgnatov <116702403+MichaelIgnatov@users.noreply.github.com>
2023-01-08 19:22:01 +0300

    Add files via upload

--- main_menu.py
+++ main_menu.py
@@ -2,6 +2,7 @@ import pygame
 from functions import terminate, place_text
 from button import Button
 from load_image import load_image
+from game_rule_screen import game_rules_screen
 
 
 def main_menu(screen, WIDTH, HEIGHT, clock, FPS):
@@ -18,10 +19,12 @@ def main_menu(screen, WIDTH, HEIGHT, clock, FPS):
     image1 = load_image('level1.png')
     image2 = load_image('level2.png')
     image3 = load_image('level3.png')
+    image4 = load_image('Rules.png')
 
     level1_button = Button(image1, (340, 200), 'level1.txt')
     level2_button = Button(image2, (355, 300), 'level2.txt')
     level3_button = Button(image3, (355, 400), 'level3.txt')
+    rules_button = Button(image4, (350, 500))
 
     menu_running = True
     while menu_running:
@@ -33,6 +36,7 @@ def main_menu(screen, WIDTH, HEIGHT, clock, FPS):
         level1_button.update(screen)
         level2_button.update(screen)
         level3_button.update(screen)
+        rules_button.update(screen)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -53,6 +57,9 @@ def main_menu(screen, WIDTH, HEIGHT, clock, FPS):
                         menu_running = False
                     else:
                         text = 'Вы собрали недостаточное количество монет'
+                if rules_button.checkForInput(mouse_pos):
+                    game_rules_screen(screen, WIDTH, HEIGHT, clock, FPS)
+
 
         pygame.display.flip()
         clock.tick(FPS)
